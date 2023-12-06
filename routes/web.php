a707271f885852a9e4896330dd8e413b81c7c87b
Mukengeshayi <desiremukengeshayi27@gmail.com>
2023-12-05 23:22:59 -0800

    template design

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AuthController::class, 'login'])->name('login');
 Route::post('/',[AuthController::class, 'handleLogin'])->name('handleLogin');
 
 # secure Route
-Route::get('dashbord',[AppController::class, 'index'])->name('dashboard');
+Route::get('/dashbord',[AppController::class, 'index'])->name('dashboard');
