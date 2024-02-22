071733aa432e17ced3e717da47dbcd56035cf961
Mukengeshayi <desiremukengeshayi27@gmail.com>
2024-02-22 08:11:11 -0800

    Create AdminController

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAdminRequest;
+use App\Http\Requests\UpdateAdminRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use PhpParser\Node\Stmt\TryCatch;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+         //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdminRequest $request)
+    {
+        $user= new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make('default');
+
+        $user->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('admin.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdminRequest $request, User $user)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreAdminRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAdminRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>'required|',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required'=>'le nom de l\'adminstrateur requis',
+            'email.required'=>'le mail est requis',
+            'email.unique'=>'le mail est deja pris',
+            'email.email'=>'le mail est invalide',
+            'password.required'=>'le mot de passe est requis',
+
+        ];
+    }
+}
